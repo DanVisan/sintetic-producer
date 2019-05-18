9e3a4c6d551d84e6648cdfcb68d7701025cf928d
Dan <visan.dan.constantin@gmail.com>
2019-05-18 12:33:46 +0300

    first commit

---
+++ src/main/java/ro/vis/sinteticproducer/SinteticProducerApplication.java
@@ -0,0 +1,13 @@
+package ro.vis.sinteticproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinteticProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SinteticProducerApplication.class, args);
+	}
+
+}
